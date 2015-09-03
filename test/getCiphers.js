97bf30510f1ef2143815ed976a3a01720e16a27a
hex7c0 <carniellifrancesco@gmail.com>
2015-09-03 22:22:55 +0200

    test: encoding assert

--- test/getCiphers.js
+++ test/getCiphers.js
@@ -37,6 +37,8 @@ describe('ciphers', function() {
         cipher: c[1][i]
       });
       assert.ok(co);
+      assert.equal(typeof co.encrypt, 'function');
+      assert.equal(typeof co.decrypt, 'function');
     }
     done();
   });
@@ -49,7 +51,56 @@ describe('ciphers', function() {
         cipher: c[2][i]
       });
       assert.ok(co);
+      assert.equal(typeof co.encrypt, 'function');
+      assert.equal(typeof co.decrypt, 'function');
     }
+    done();
+  });
+  it('should build with different encoding', function(done) {
+
+    var vault0 = cookiee('hello_world!', {
+      encoding: 'ascii'
+    });
+    assert.equal(vault0.encoding, 'ascii');
+
+    var vault1 = cookiee('hello_world!', {
+      encoding: 'utf8'
+    });
+    assert.equal(vault1.encoding, 'utf8');
+
+    var vault2 = cookiee('hello_world!', {
+      encoding: 'utf16le'
+    });
+    assert.equal(vault2.encoding, 'utf16le');
+
+    var vault3 = cookiee('hello_world!', {
+      encoding: 'ucs2'
+    });
+    assert.equal(vault3.encoding, 'ucs2');
+
+    var vault4 = cookiee('hello_world!', {
+      encoding: 'base64'
+    });
+    assert.equal(vault4.encoding, 'base64');
+
+    var vault5 = cookiee('hello_world!', {
+      encoding: 'binary'
+    });
+    assert.equal(vault5.encoding, 'binary');
+
+    var vault6 = cookiee('hello_world!', {
+      encoding: 'hex'
+    });
+    assert.equal(vault6.encoding, 'hex');
+
+    assert.equal(vault0.encrypt('f'), vault0.encrypt('f'));
+    // assert.notEqual(vault0.encrypt('f'), vault1.encrypt('f'));
+    assert.notEqual(vault0.encrypt('f'), vault2.encrypt('f'));
+    assert.notEqual(vault0.encrypt('f'), vault3.encrypt('f'));
+    assert.notEqual(vault0.encrypt('f'), vault4.encrypt('f'));
+    // assert.notEqual(vault0.encrypt('f'), vault5.encrypt('f'));
+    assert.notEqual(vault0.encrypt('f'), vault6.encrypt('f'));
+
     done();
   });
 });
